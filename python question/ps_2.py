71b79b07754933eaf8d4bd16aa0a38b2d333997d
Shreyas Walde <waldeshreyas@gmail.com>
2023-12-21 10:52:05 +0530

    ps_2.py

--- python question/ps_2.py
+++ python question/ps_2.py
@@ -1,3 +1,3 @@
-var_a = input(int("Enter a number: "))
+var_a = input("Enter a number: ")
 var_b = int(var_a) % 2
-print(f"Remainder of the number is: + int(var_b)" )
+print(f"Remainder of the number is:  {int(var_b)}")
