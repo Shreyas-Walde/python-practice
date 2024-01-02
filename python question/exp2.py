f79409086c8c60a9cfdd1c9c30ce0d402c783bd7
Shreyas Walde <waldeshreyas@gmail.com>
2024-01-02 19:15:19 +0530

    Updated Readme

--- python question/exp2.py
+++ python question/exp2.py
@@ -8,7 +8,6 @@ print("Welcome to the PyPassword Generator!")
 nr_letters= int(input("How many letters would you like in your password?\n")) 
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
-
 #Eazy Level - Order not randomised:
 #e.g. 4 letter, 2 symbol, 2 number = JduE&!91
 # password = ""
@@ -22,7 +21,6 @@ nr_numbers = int(input(f"How many numbers would you like?\n"))
 #     password += random.choice(symbols)
     
 # print(password)
-
 
 #Hard Level - Order of characters randomised:
 #e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
